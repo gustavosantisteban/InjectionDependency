0cda2c89dd57cde182726dfb93a42aa5bef9d7d9
William Gustavo Santisteban <rootsantisteban@gmail.com>
2017-02-10 15:45:34 -0300

    Inyeccion de Dependencias
    Introduccion a la Inyeccion de Dependencias con Ninject

---
+++ DIExample/DIEjemplo/DIEjemplo/Bienvenida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIEjemplo
+{
+    public class Bienvenida
+    {
+        public void Saludo()
+        {
+            Console.WriteLine("Hola desde Ninject");
+        }
+    }
+}
---
+++ DIExample/DIEjemplo/DIEjemplo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+namespace DIEjemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var kernel = new StandardKernel())
+            {
+                var service = kernel.Get<Bienvenida>();
+                service.Saludo();
+                Console.ReadLine();
+            }
+        }
+    }
+}
