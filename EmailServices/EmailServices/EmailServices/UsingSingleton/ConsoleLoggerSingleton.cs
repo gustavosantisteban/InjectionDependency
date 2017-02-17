e62c2488f2977bb71a0b5682254acfd039ffd92d
William Gustavo Santisteban <rootsantisteban@gmail.com>
2017-02-17 17:29:26 -0300

    Singleton

---
+++ EmailServices/EmailServices/EmailServices/UsingSingleton/ConsoleLoggerSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailServices.UsingSingleton
+{
+    class ConsoleLoggerSingleton : ILogger
+    {
+        private static ConsoleLoggerSingleton instance = new ConsoleLoggerSingleton();
+        
+        static ConsoleLoggerSingleton()
+        {
+
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("{0}: {1}", DateTime.Now, message);
+        }
+    }
+}
---
+++ EmailServices/EmailServices/EmailServices/UsingSingleton/EventLoggerSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailServices.UsingSingleton
+{
+    class EventLoggerSingleton : ILogger
+    {
+
+        private static EventLoggerSingleton instance = new EventLoggerSingleton();
+
+        static EventLoggerSingleton()
+        {
+
+        }
+
+        public void Log(string message)
+        {
+            EventLog.WriteEntry("EmailService", message);
+        }
+    }
+}
